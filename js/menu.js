ba9e5ac2ac1b0f2ca5357afa0004faec751425e8
tipyaf <ya.net@hotmail.fr>
2016-04-17 22:33:54 +0200

    add smooth scroll + padding section

--- js/menu.js
+++ js/menu.js
@@ -8,3 +8,12 @@ $(document).ready(function(){
     });
 
 });
+
+$(document).ready(function() {
+    $('.toAcceuil,.toProjects, .toCv, .toHobbies, .toContacts').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 2100; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+});
