7a932fac1f175709a835202418f895ecd236841d
tipyaf <ya.net@hotmail.fr>
2016-04-18 00:40:05 +0200

    add new stop.js

---
+++ js/stop.js
@@ -0,0 +1,86 @@
+/**
+ * Created by yannickbenchimol on 18/04/2016.
+ */
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+    // create the global player from the specific iframe (#video)
+
+    //cv video
+    player = new YT.Player('video', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
+        }
+    });
+    //analistik video
+    playerAna = new YT.Player('ana', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
+        }
+    });
+    //coup de coeur video (add id + ?enablejsapi=1 to html src)
+    playerFada = new YT.Player('fada', {
+        events: {
+            // call this function when player is ready to use
+            'onReady': onPlayerReady
+        }
+    });
+
+}
+
+function onPlayerReady(event) {
+
+    // bind events
+    var playButton = document.getElementById("cvVid");
+    playButton.addEventListener("click", function() {
+        player.playVideo();
+    });
+
+    var stopButtonCv = document.getElementById("md-close");
+    stopButtonCv.addEventListener("click", function() {
+        player.stopVideo();
+    });
+    var stopButtonMusic = document.getElementById("hobbies-stop");
+    stopButtonMusic.addEventListener("click", function() {
+        playerAna.stopVideo();
+        playerFada.stopVideo();
+
+    });
+    var stopButtonMusic = document.getElementById("hobbies-stopVid");
+    stopButtonMusic.addEventListener("click", function() {
+        playerAna.stopVideo();
+        playerFada.stopVideo();
+
+    });
+
+
+}
+
+// Inject YouTube API script
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+//soundcloud stop on close (add 'nyt' id to iframe html)
+
+$(function(){
+
+    var iframeElement   = document.querySelector('iframe');
+    var iframeElementID = iframeElement.id;
+    var widget1         = SC.Widget("nyt");
+
+
+
+
+
+    $("#hobbies-stop,#hobbies-stopVid").click(function() {
+        widget1.pause()
+    });
+
+})
