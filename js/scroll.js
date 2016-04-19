c8feec412b92a9ad1a6e361be20cc74bf3320744
tipyaf <ya.net@hotmail.fr>
2016-04-19 22:21:26 +0200

    add animated scrolling

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+
+wow = new WOW(
+    {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       false,       // default
+        live:         true        // default
+    }
+)
+new WOW().init();
