2d4085c07431cfd11e65f165dd05f39d1e94be53
tipyaf <ya.net@hotmail.fr>
2016-04-28 00:57:42 +0200

    add clipboard contact

--- js/contacts.js
+++ js/contacts.js
@@ -2,9 +2,19 @@
  * Created by ya on 17/04/16.
  */
 
-$(".tel").click(function(){
-    $(".tel div").text("07 60 64 93 53").addClass("animated zoomIn");
-});
-$(".mail").click(function(){
-    $(".mail div").text("ya.net@hotmail.fr").addClass("animated zoomIn");
-});
+    $("#phone").text("07 60 64 93 53");
+
+
+    var mail = "ya.net@hotmail.fr"
+    $("#mail").text(mail);
+
+    $(".copymail").click(function(){
+        $('.copyTxtMail').text('Copiée');
+    });
+
+    $(".copyphone").click(function(){
+    $('.copyTxtPhone').text('Copié');
+    });
+
+var clipboardPhone = new Clipboard('.copyTxtPhone');
+var clipboardMail = new Clipboard('.copyTxtMail');
